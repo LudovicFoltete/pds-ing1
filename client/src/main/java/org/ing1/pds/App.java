9b8c3c87d2a8a6123cdefce90a33cfea2273b1fb
Foltete Ludovic <ludovicflb@gmail.com>
2019-02-07 20:47:09 +0100

    test communication client serveur

--- client/src/main/java/org/ing1/pds/App.java
+++ client/src/main/java/org/ing1/pds/App.java
@@ -1,13 +1,50 @@
 package org.ing1.pds;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Socket socket;
+        int port = 2121;
+
+        InetAddress serverAddress;
+
+        try {
+            // get the firewall's IP address
+            serverAddress = InetAddress.getByName("firewall");
+
+            // request a connection to the server
+            socket = new Socket(serverAddress, port);
+            System.out.println("Connected !");
+
+            // get the input and output streams for this socket
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+            // communication with the client
+            out.println("Hi ! I'm the client !");
+            out.flush();
+            System.out.println(in.readLine());
+            System.out.println(in.readLine());
+
+            // close all
+            in.close();
+            out.close();
+            socket.close();
+
+        }catch (UnknownHostException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- server/src/main/java/org/ing1/pds/App.java
+++ server/src/main/java/org/ing1/pds/App.java
@@ -1,13 +1,48 @@
 package org.ing1.pds;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ServerSocket serverSocket  ;
+        Socket socket ;
+        int port = 2121;
+
+        try {
+
+            // server initialization on port specified
+            serverSocket = new ServerSocket(port);
+            System.out.println("The server is listening to the port " + port);
+
+            // waiting for connection from the client
+            socket = serverSocket.accept();
+            System.out.println("New connection accepted !");
+
+            // get the input and output stream for this socket
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            //communication with the client
+            System.out.println(in.readLine());
+            out.println("Hi, I'm the server !");
+            out.println("Nice to meet you !");
+            out.flush();
+
+            // close all
+            in.close();
+            out.close();
+            serverSocket.close();
+            socket.close();
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
