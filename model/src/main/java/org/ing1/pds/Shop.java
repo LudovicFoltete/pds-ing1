cace2f194ad12507200b60621f928b4ca7a9f1d2
Foltete Ludovic <ludovicflb@gmail.com>
2019-02-09 18:02:08 +0100

    Ajout du JavaBean Shop dans model

---
+++ model/src/main/java/org/ing1/pds/Shop.java
@@ -0,0 +1,64 @@
+package org.ing1.pds;
+
+import java.io.Serializable;
+
+public class Shop implements Serializable {
+
+    private int id;
+    private String name;
+    private String category;
+    private int location_id;
+    private String phone;
+    private String email;
+
+    public Shop() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getLocation_id() {
+        return location_id;
+    }
+
+    public void setLocation_id(int location_id) {
+        this.location_id = location_id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
